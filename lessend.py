ada1cd67b54a1ebdb1b4cbd7543e77034c5020f7
Artur470 <arturunkulov30@gmail.com>
2023-11-28 23:22:03 +0600

    versia,a

---
+++ lessend.py
@@ -0,0 +1,229 @@
+
+"""
+
+class Africa:
+    def __init__(self, arg_lion, arg_giraffe, arg_crocodile, arg_zebra,
+                       arg_jakal, arg_rhinoceros, arg_elephant, arg_name):
+
+        self.__lion = arg_lion
+        self.__giraffe = arg_giraffe
+        self.__crocodile = arg_crocodile
+        self.__zebra = arg_zebra
+        self.__jakal = arg_jakal
+        self.__rhinoceros = arg_rhinoceros
+        self.__elephant = arg_elephant
+        self.__name = arg_name
+
+
+    def lion_is_the_king_of_the_animals(self):
+        print(f'lion_and_di_hunt_for_any_promise_of_africa!!!')
+
+    def giraffe_walks_with_both_rigth_legs_at_the_same_time(self):
+        print(f'giraffe_run_fast_with_both_feet!!!')
+
+    def the_crocodile_has_a_very_strong_jaw_and_teeth(self):
+        print(f'crocodile_bites_off_one_of_the_victims_legs_the_first_time!!!')
+
+    def zebra_has_very_good_hearing(self0):
+        print(f'zebra_protects_its_family_froom_danger!!!')
+
+    def a_jacal_can_store_food_in_itself(self):
+        print(f'jacal_hroni_food_supply_for_a_month!!!')
+
+    def rhinoceros_has_goot_vision_power(self):
+        print(f'rhinoceros_go_turn_the_car_over!!!')
+
+    def elephant_cant_jump(self):
+        print(f'elephant_learn_to_jump!!!')
+
+
+
+    def get_lion(self):
+        return self.__lion
+
+    def get_giraffe(self):
+        return self.__giraffe
+
+    def get_crocodile(self):
+        return self.__crocodile
+
+    def get_zebra(self):
+        return self.__zebra
+
+    def get_jakal(self):
+        return self.__jakal
+
+    def get_rhinoceros(self):
+        return self.__rhinoceros
+
+    def get_elephant(self):
+        return self.__elephant
+
+
+    def set_lion(self, new_lion):
+        self.__lion = new_lion
+
+    def set_giraffe(self, new_giraffe):
+        self.__giraffe = new_giraffe
+
+    def set_crocodile(self, new_crocodile):
+        self.__crocodile = new_crocodile
+
+    def set_zebra(self, new_zebra):
+        self.__zebra = new_zebra
+
+    def set_jakal(self, new_jakal):
+        self.__zebra = new_jakal
+
+    def set_rhinoceros(self, new_rhinoceros):
+        self.__rhinoceros = new_rhinoceros
+
+    def set_elephant(self, new_elephant):
+        self.__elephant = new_elephant
+
+
+    def show_info(self):
+        print(f'name:  {self.__name}'
+              f'lion : {self.get_lion()}\n'
+              f'giraffe: {self.get_giraffe()}\n'
+              f'crocodile: {self.get_crocodile()}\n'
+              f'zebra: {self.get_zebra()}\n'
+              f'jacal: {self.get_jakal()}\n'
+              f'ghinoceros: {self.get_rhinoceros()}\n'
+              f'elephant: {self.get_elephant()}\n')
+
+
+
+savanna = Africa('lion', 'giraffe', 'crocodile', 'zebra',
+
+                 'jakal', 'rhinoceros','elephant ', 'savanna')
+
+
+savanna.show_info()
+
+savanna.lion_is_the_king_of_the_animals()
+savanna.giraffe_walks_with_both_rigth_legs_at_the_same_time()
+savanna.the_crocodile_has_a_very_strong_jaw_and_teeth()
+savanna.zebra_has_very_good_hearing()
+savanna.a_jacal_can_store_food_in_itself()
+savanna.rhinoceros_has_goot_vision_power()
+savanna.elephant_cant_jump()
+
+"""
+
+"""
+
+class Car:
+    def __init__(self, arg_mator, arg_amartezator, arg_svecha, arg_fara, arg_kardanvall,
+                 arg_kaleso, arg_kapot, arg_conditsioner , arg_name)
+
+
+
+     self.__mator = arg_mator
+
+     self.__amartezator = arg_amartezator
+
+     self.__svecha = arg_svecha
+
+     self.__fara = arg_fara
+
+     self.__kardanvall = arg_kardanvall
+
+     self.__kaleso = arg_kaleso
+
+     self.__kapot = arg_kapot
+
+     self.__conditsioner = arg_conditsioner
+
+     self.__name = arg_name
+
+
+    def mator_dvigaet_mashinu_s_mesta(self):
+        print(f'mator_s_maxsemalnoy_moshnosty!!!')
+
+    def amartezator_pomogaet_derjat_bolans(self):
+        print(f'amortezator_derji_bolans_pri_vosokoy_skorosti!!!')
+
+    def svecha_zojiganie_neobhodim_chtop_zavodit_mashinu(self):
+        print(f'svecha_zavodi_mashinu!!!')
+
+    def fara_nujna_dly_osvishennie_nochy(self):
+        print(f'fara_dalnoe_osvishenie!!!')
+
+    def bez_kardanvalla_dvijenie_nevozmojno(self):
+        print(f'kardanvall_pomagay_mashine_pri_dvijenii!!!')
+
+    def coleso_neobhodim_dly_mashiny(self):
+        print(f'coleso_but_horoshom_sostoynii_pri_dvijenii!!!')
+
+    def kapot_nujen_chtop_mator_hronit_bezapasnosti(self):
+        print(f'kapot_sohrony_detali_ot_dojdy!!!')
+
+    def conditsioner_pomogaet_v_nujnuy_pogodu(self):
+        print(f'conditsioner_vklychi_v_zimnem_rejime!!!')
+
+
+    def get_mator(self):
+        return self.__mator
+
+    def get_amartezator(self):
+        return self.__amartezator
+
+    def get_svecha(self):
+        return self.__svecha
+
+    def get_fara(self):
+        return self.__fara
+
+    def get_kardanvall(self):
+        return self.__kardanvall
+
+    def get_coleso(self):
+        return self.__kaleso\
+
+    def get_kapot(self):
+        return self.__kapot
+
+    def get_conditsioner(self):
+        return self.__conditsioner
+    def get_name(self):
+        return self.__name
+
+
+    def set_mator(self, new_mator):
+        self.__mator = new_mator
+
+
+    def set_amaortezator(self, new_amartezator):
+        self.__amartezator = new_amartezator
+
+
+    def set_svecha(self, new_svecha):
+        self.__svecha = new_svecha
+
+
+    def set_fara(self, new_fara):
+        self.__fara = new_fara
+
+    def set_kardanvall(self, new_kardanvall):
+        self.__kardanvall = new_kardanvall
+    def set_name(self, new_car):
+        self.__
+
+    def set_coleso(self, new_coleso):
+        self.__coleso = new_coleso
+
+    def set_kapot(self, new_kapot):
+        self.__kapot = new_kapot
+
+    def set_conditsioner(self, new_conditsioner):
+        self.__conditsioner = new_conditsioner
+
+Car = Car('kapot', 'fara', 'kardanvall' 'car', "")
+
+сar.kapot_nujen_chtop_mator_hronit_bezapasnosti()
+сar.fara_nujna_dly_osvishennie_nochy()
+сar. bez_kardanvalla_dvijenie_nevozmojno()
+"""
+
+
